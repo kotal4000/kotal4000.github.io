b43d60835cf6d1bd9183415a9b68177e180bb24d
kotal4000 <123707158+kotal4000@users.noreply.github.com>
2023-01-27 19:48:25 +0000

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,17 @@
+let mybutton = document.getElementById("nav-button");
+
+        window.onload = mybutton.style.display = "none";
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+        }
+
+        function topFunction() {
+        document.body.scrollTop = 0; // Safari
+        document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
+        }
