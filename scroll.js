54f1b9e3b6c16c74a3c20d016bd9515e2eb6ae6c
kotal4000 <123707158+kotal4000@users.noreply.github.com>
2023-01-28 02:29:07 +0000

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -4,7 +4,7 @@ let mybutton = document.getElementById("nav-button");
         window.onscroll = function() {scrollFunction()};
 
         function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
             mybutton.style.display = "block";
         } else {
             mybutton.style.display = "none";
